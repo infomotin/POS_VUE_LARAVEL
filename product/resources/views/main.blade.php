de9d31b76918ab21a4036234c9687658bcd175dd
Abdul Motin <motin.mmk.jr@gmail.com>
2022-06-26 11:49:09 +0600

    Database schema lenth limit  migrate controller view

---
+++ product/resources/views/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- add bootstrap css --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Laravel 7 Home Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Laravel 7 Home Page</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Add Your Product here</h1>
+                {{-- button with href --}}
+                <a href="" class="btn btn-primary">Add Product</a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+               
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
